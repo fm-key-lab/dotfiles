387413ac57ddcceab769b2569417bea1ce2eccc0
Thomas Silvers <thomas.silvers@me.com>
2025-01-05 16:20:26 +0100

    Add container work dir for nxf

--- .bashrc.d/51_apptainer.sh
+++ .bashrc.d/51_apptainer.sh
@@ -2,9 +2,10 @@
 
 # TODO: Use a config file, set up per descriptions [here](https://apptainer.org/docs/user/main/bind_paths_and_mounts.html#bind-paths-and-mounts)
 
-# NOTE: Should not use `\tmp` (see [here](https://docs.mpcdf.mpg.de/doc/computing/raven-user-guide.html#tmp))
-
 export APPTAINER_TMPDIR="/ptmp/$USER/apptainer-build" # NOTE that these get overwritten by MPCDFs apptainer modulefiles
+export APPTAINERENV_TMP="/ptmp/$USER/apptainer-build"
 export APPTAINERENV_TMPDIR="/ptmp/$USER/apptainer-build"
 
-export SINGULARITYENV_TMPDIR="/ptmp/$USER/apptainer-build" # backwards compatibility
+# backwards compatibility
+export SINGULARITYENV_TMP="/ptmp/$USER/apptainer-build"
+export SINGULARITYENV_TMPDIR="/ptmp/$USER/apptainer-build"

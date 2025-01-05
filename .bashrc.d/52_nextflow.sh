01cee8bcdcba9b2b748b697c64c40cd4a00faaae
Thomas Silvers <thomas.silvers@me.com>
2025-01-05 16:20:46 +0100

    Add container work dir for nxf

--- .bashrc.d/52_nextflow.sh
+++ .bashrc.d/52_nextflow.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-export APPTAINERENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug"
-export SINGULARITYENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug" # backwards compatibility
+# Java memory
 
 export NXF_OPTS='-Xms1g -Xmx4g' # legacy (nf<21)
 export NXF_JVM_ARGS='-Xms1g -Xmx4g' # nf>=21
 
-# TODO: Not sure which is best here ...
+# Containers
 
-# export NXF_SINGULARITY_CACHEDIR="/ptmp/$USER/apptainer-cache"
+export APPTAINERENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug"
+export APPTAINERENV_NXF_TASK_WORKDIR="/ptmp/$USER/apptainer-nxf-work"
 # export NXF_APPTAINER_CACHEDIR="/ptmp/$USER/apptainer-cache"
-
-export NXF_SINGULARITY_CACHEDIR="$GROUP_HOME/containers/nxf-cache"
 export NXF_APPTAINER_CACHEDIR="$GROUP_HOME/containers/nxf-cache"
+
+# backwards compatibility
+export SINGULARITYENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug"
+export SINGULARITYENV_NXF_TASK_WORKDIR="/ptmp/$USER/apptainer-nxf-work"
+# export NXF_SINGULARITY_CACHEDIR="/ptmp/$USER/apptainer-cache"
+export NXF_SINGULARITY_CACHEDIR="$GROUP_HOME/containers/nxf-cache"

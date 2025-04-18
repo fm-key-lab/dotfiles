aa7851cefd018933f6e610e0df75af9b9567f4f3
t-silvers <thomas.silvers@me.com>
2025-04-18 17:17:23 +0200

    Add clean up functions

--- .bashrc.d/42_cleanup.sh
+++ .bashrc.d/42_cleanup.sh
@@ -5,20 +5,20 @@
 # Globals: none
 # Input: nothing
 # Output: nothing
-function Clean_Conda {
+function __Clean_Conda {
     # See:  https://docs.conda.io/projects/conda/en/stable/commands/clean.html
     echo "Cleaning up conda"
     module load anaconda/3/2023.03
     conda clean --all -y
     module unload anaconda/3/2023.03
-} # End of function bar
+}
 
 #--------------------------------------------------------------------------
 # Clean up the Nextflow cache
 # Globals: none
 # Input: nothing
 # Output: nothing
-function Clean_Nextflow {
+function __Clean_Nextflow {
     # See:  https://www.nextflow.io/docs/latest/reference/cli.html#clean
     #       https://www.nextflow.io/docs/latest/cache-and-resume.html
     echo "Cleaning up nextflow"
@@ -26,92 +26,81 @@ function Clean_Nextflow {
     # module load nextflow
     # nextflow clean -f
     # module unload nextflow
-} # End of function bar
+}
 
 #--------------------------------------------------------------------------
 # Clean up the renv cache
 # Globals: none
 # Input: nothing
 # Output: nothing
-function Clean_Renv {
+function __Clean_Renv {
     # See:  https://rstudio.github.io/renv/articles/package-install.html#cache
     # TODO:
     echo "Cleaning up renv"
-} # End of function bar
+}
 
 #--------------------------------------------------------------------------
 # Clean up the uv cache
 # Globals: none
 # Input: nothing
 # Output: nothing
-function Clean_UV {
+function __Clean_UV {
     # See:  https://docs.astral.sh/uv/reference/cli/#uv-cache-clean
     #       https://docs.astral.sh/uv/concepts/cache/
     echo "Cleaning up uv"
     module load uv
     uv cache clean
     module unload uv
-} # End of function bar
+}
 
 #--------------------------------------------------------------------------
 # Clean up the VS Code Server on the remote
 # Globals: none
 # Input: nothing
 # Output: nothing
-function Clean_VSCode_Server {
+function __Clean_VSCode_Server {
     # See:  https://code.visualstudio.com/docs/remote/troubleshooting#_cleaning-up-the-vs-code-server-on-the-remote
     echo "Cleaning up vs code servers"
     pgrep -u $USER -f "vscode-server" | while read pid; do
         kill -9 $pid
     done
     rm -rf "$HOME/.vscode-server" # Or ~/.vscode-server-insiders
-} # End of function CleanupVSCodeServer
+}
 
 #--------------------------------------------------------------------------
 # Clean up everything
 # Globals: none
 # Input: nothing
 # Output: nothing
-function Clean_Up {
-    Clean_VSCode_Server
-
-    # If $HOME quota is exceeded, may have trouble loading modules,
-    # so remove vs code server first (no modules required).
-    Clean_Conda
-    Clean_Nextflow
-    Clean_Renv
-    Clean_UV
-}
-
-function Clean_Up () {
+function Cleanup () {
   case "$1" in
     conda)
-      Clean_Conda || exit
+      __Clean_Conda || exit
       ;;
     everything)
       # If $HOME quota is exceeded, may have trouble loading modules,
       # so remove vs code server first (no modules required).
-      Clean_VSCode_Server \
-      && Clean_Conda \
-      && Clean_Nextflow \
-      && Clean_Renv \
-      && Clean_UV \
+      __Clean_VSCode_Server \
+      && __Clean_Conda \
+      && __Clean_Nextflow \
+      && __Clean_Renv \
+      && __Clean_UV \
       || exit
       ;;
     nextflow)
-      Clean_Nextflow || exit
+      __Clean_Nextflow || exit
       ;;
     renv)
-      Clean_Renv || exit
+      __Clean_Renv || exit
       ;;
     uv)
-      Clean_UV || exit
+      __Clean_UV || exit
       ;;
     vscode)
-      Clean_VSCode_Server || exit
+      __Clean_VSCode_Server || exit
       ;;
     *)
-      echo "Clean_Up: unknown key '$1'"
+      echo "Cleanup: unknown key '$1'"
       return 1
       ;;
   esac
@@ -119,4 +108,4 @@ function Clean_Up () {
 }
 
 # Set up tab completion
-complete -W "conda everything nextflow renv uv vscode" Clean_Up
+complete -W "conda everything nextflow renv uv vscode" Cleanup

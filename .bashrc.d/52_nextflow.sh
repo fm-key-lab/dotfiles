7ba9c30723b59cd9339e5337f0776db5a7f1a666
t-silvers <thomas.silvers@me.com>
2025-04-23 14:48:50 +0200

    Add nextflow config for java virtual threads

--- .bashrc.d/52_nextflow.sh
+++ .bashrc.d/52_nextflow.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# Java memory
+# See "Optimizing Nextflow for HPC and cloud at scale" at
+# https://seqera.io/blog/optimizing-nextflow-for-hpc-and-cloud-at-scale/
 
+#
+# Java virtual machines memory
+#
+
+# Provides extra options for the Java and Nextflow runtime. It must be a blank 
+# separated list of -Dkey[=value] properties.
 export NXF_OPTS='-Xms1g -Xmx4g' # legacy (nf<21)
-export NXF_JVM_ARGS='-Xms1g -Xmx4g' # nf>=21
 
+# Allows the setting Java VM options. This is similar to NXF_OPTS however it’s only 
+# applied the JVM running Nextflow and not to any java pre-launching commands.
+export NXF_JVM_ARGS='-Xms1g -Xmx4g' # nf>=21.12.1-edge
+
+# Enable the use of virtual threads in the Nextflow runtime (default: false)
+# See: https://github.com/nextflow-io/nextflow/discussions/4711
+export NXF_ENABLE_VIRTUAL_THREADS=false # nf>=23.05.0-edge
+# ^ Also ensure using nextflow with java>=21, which enables
+# virtual threads by default.
+
+#
 # Containers
+#
 
 export APPTAINERENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug"
-export APPTAINERENV_NXF_TASK_WORKDIR="/ptmp/$USER/apptainer-nxf-work"
-# export NXF_APPTAINER_CACHEDIR="/ptmp/$USER/apptainer-cache"
-export NXF_APPTAINER_CACHEDIR="$GROUP_HOME/containers/nxf-cache"
+export SINGULARITYENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug" # backwards compatibility
 
-# backwards compatibility
-export SINGULARITYENV_NXF_DEBUG="/ptmp/$USER/apptainer-debug"
-export SINGULARITYENV_NXF_TASK_WORKDIR="/ptmp/$USER/apptainer-nxf-work"
-# export NXF_SINGULARITY_CACHEDIR="/ptmp/$USER/apptainer-cache"
-export NXF_SINGULARITY_CACHEDIR="$GROUP_HOME/containers/nxf-cache"
+export APPTAINERENV_NXF_TASK_WORKDIR="/ptmp/$USER/apptainer-nxf-work"
+export SINGULARITYENV_NXF_TASK_WORKDIR="/ptmp/$USER/apptainer-nxf-work" # backwards compatibility
+
+# Using a shared location for *_CACHEDIR provides access to containers for 
+# local, offline versions of nf-core pipelines
+export NXF_APPTAINER_CACHEDIR="$GROUP_HOME/containers/nxf-cache"
+export NXF_SINGULARITY_CACHEDIR="$GROUP_HOME/containers/nxf-cache" # backwards compatibility
